4ba947f0ccdad54c4a0df11c4977e54e30fdd67d
token <239573049@qq.com>
2025-04-19 14:24:40 +0800

    Update memory package references and add configuration
    Upgrade SemanticKernel connectors to newer versions and introduce additional memory support including InMemory, Milvus, and Weaviate. Add new memory configuration options in `appsettings.json` and extend service extensions for various memory types.

--- kolala-memory/KoalaMemory/ServiceExtensions.cs
+++ kolala-memory/KoalaMemory/ServiceExtensions.cs
@@ -1,11 +1,65 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Data.Sqlite;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.Connectors.Redis;
+using Microsoft.SemanticKernel.Connectors.Weaviate;
 using Npgsql;
+using StackExchange.Redis;
 
 namespace Koala_Memory;
 
 public static class ServiceExtensions
 {
+    
+    public static IServiceCollection AddInMemoryKoalaMemory(this IServiceCollection service)
+    {
+        service.AddInMemoryVectorStore();
+
+        return service;
+    }
+
+    public static IServiceCollection AddWeaviateKoalaMemory(this IServiceCollection service, string apiKey,
+        string connectionsString)
+    {
+        service.AddWeaviateVectorStore(options: new WeaviateVectorStoreOptions()
+        {
+            Endpoint = new Uri(connectionsString),
+            ApiKey = apiKey
+        });
+
+        return service;
+    }
+
+    public static IServiceCollection AddRedisKoalaMemory(this IServiceCollection service, string connectionsString)
+    {
+        service.AddSingleton<IDatabase>(sp => ConnectionMultiplexer.Connect(connectionsString).GetDatabase());
+
+        service.AddRedisVectorStore(options: new RedisVectorStoreOptions()
+        {
+            StorageType = RedisStorageType.Json
+        });
+
+        return service;
+    }
+
+    public static IServiceCollection AddSqliteKoalaMemory(this IServiceCollection service, string connectionsString,
+        string vectorName)
+    {
+        service.AddSingleton<SqliteConnection>(sp =>
+        {
+            var connection = new SqliteConnection(connectionsString);
+
+            connection.LoadExtension(vectorName);
+
+            return connection;
+        });
+
+        service.AddSqliteVectorStore();
+
+
+        return service;
+    }
+
     public static IServiceCollection AddPostgresKoalaMemory(this IServiceCollection services, string connectionString)
     {
         services.AddSingleton<NpgsqlDataSource>(sp =>
